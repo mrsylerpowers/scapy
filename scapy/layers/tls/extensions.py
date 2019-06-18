f0031c0ad7a8f0dccf94ad6347980ce912f49fa5
Justice Parham <justking100@gmail.com>
2019-06-18 13:53:57 -0500

    Removed Whitespace

--- scapy/layers/tls/extensions.py
+++ scapy/layers/tls/extensions.py
@@ -150,14 +150,16 @@ class ServerName(Packet):
     def guess_payload_class(self, p):
         return Padding
 
+
 class EncryptedServerName(Packet):
     name = "Encrypted HostName"
     fields_desc = [ShortEnumField("type", 0xffce, _tls_ext),
                    ShortField("len", None),
                    EnumField("cipher", None, _tls_cipher_suites),
-                   ShortEnumField("key_exchange_group", None, _tls_named_groups),
+                   ShortEnumField("key_exchange_group", None,
+                                _tls_named_groups),
                    FieldLenField("key_exchange_len", None,
-                               length_of="key_exchange",fmt="H"),
+                               length_of="key_exchange", fmt="H"), 
                    XStrLenField("key_exchange", "",
                                length_from=lambda pkt: pkt.key_exchange_len),
                    FieldLenField("record_digest_len", None, length_of="record_digest"),
@@ -166,28 +168,6 @@ class EncryptedServerName(Packet):
                    FieldLenField("encrypted_sni_len", None,
                                length_of="encrypted_sni",fmt="H"),
                    XStrLenField("encrypted_sni", "",
-                               length_from=lambda pkt: pkt.encrypted_sni_len)]
-	
-    def guess_payload_class(self, p):
-        return Padding
-
-class EncryptedServerName(Packet):
-    name = "Encrypted HostName"
-    fields_desc = [ShortEnumField("type", 0xffce, _tls_ext), 
-                   ShortField("len", None), 
-                   EnumField("cipher", None, _tls_cipher_suites),
-                   ShortEnumField("key_exchange_group", None,
-                                _tls_named_groups), 
-                   FieldLenField("key_exchange_len", None,
-                               length_of="key_exchange",fmt="H"), 
-                   XStrLenField("key_exchange", "", 
-                               length_from=lambda pkt: pkt.key_exchange_len), 
-                   FieldLenField("record_digest_len", None, length_of="record_digest"), 
-                   XStrLenField("record_digest", "",
-                               length_from=lambda pkt: pkt.record_digest_len), 
-                   FieldLenField("encrypted_sni_len", None, 
-                               length_of="encrypted_sni",fmt="H"),
-                   XStrLenField("encrypted_sni", "", 
                                length_from=lambda pkt: pkt.encrypted_sni_len)]
 
     def guess_payload_class(self, p):
@@ -229,14 +209,14 @@ class TLS_Ext_EncryptedServerName(TLS_Ext_PrettyPacketList):
                    EnumField("cipher", None, _tls_cipher_suites),
                    ShortEnumField("key_exchange_group", None, _tls_named_groups),
                    FieldLenField("key_exchange_len", None,
-                               length_of="key_exchange",fmt="H"),
+                               length_of="key_exchange", fmt="H"),
                    XStrLenField("key_exchange", "",
                                length_from=lambda pkt: pkt.key_exchange_len),
                    FieldLenField("record_digest_len", None, length_of="record_digest"),
                    XStrLenField("record_digest", "",
                                length_from=lambda pkt: pkt.record_digest_len),
                    FieldLenField("encrypted_sni_len", None,
-                               length_of="encrypted_sni",fmt="H"),
+                               length_of="encrypted_sni", fmt="H"),
                    XStrLenField("encrypted_sni", "",
                                length_from=lambda pkt: pkt.encrypted_sni_len)]
 
